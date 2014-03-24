5aeeb26a8895f779c28908e7fba29382c240e868
Mauro Servienti <mauro@topics.it>
2014-03-24 11:14:30 +0100

    fixing wp8 after removal of wp7.x

---
+++ src/wp8/Radical.Windows/Phone/Shell/Services/LifecycleEvent.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Topics.Radical.Windows.Phone.Shell.Services
+{
+	/// <summary>
+	/// Defines the lifecycle events of a phone application.
+	/// </summary>
+	public enum LifecycleEvent
+	{
+		/// <summary>
+		/// The app is starting.
+		/// </summary>
+		Starting,
+
+		/// <summary>
+		/// The app is resuming.
+		/// </summary>
+		Resuming,
+
+		/// <summary>
+		/// The app is closing.
+		/// </summary>
+		Closing,
+
+		/// <summary>
+		/// The app is suspending.
+		/// </summary>
+		Suspending,
+
+		/// <summary>
+		/// The app has been resumed.
+		/// </summary>
+		Resumed,
+
+		/// <summary>
+		/// The app has been unobscured.
+		/// </summary>
+		Unobscured,
+
+		/// <summary>
+		/// The app has been obscured.
+		/// </summary>
+		Obscured
+	}
+}
---
+++ src/wp8/Radical/Container/BootableFacility.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Topics.Radical.ComponentModel;
+using Topics.Radical.Reflection;
+
+namespace Topics.Radical
+{
+	/// <summary>
+	/// Defines a facility that automatically boot a component at registration time.
+	/// </summary>
+	public class BootableFacility : IPuzzleContainerFacility
+	{
+		IPuzzleContainer container;
+
+		/// <summary>
+		/// Initializes this facility.
+		/// </summary>
+		/// <param name="container">The container hosting the facility.</param>
+		public void Initialize( IPuzzleContainer container )
+		{
+			this.container = container;
+			container.ComponentRegistered += new EventHandler<ComponentRegisteredEventArgs>( OnComponentRegistered );
+		}
+
+		void OnComponentRegistered( object sender, ComponentRegisteredEventArgs e )
+		{
+			if( e.Entry.Service.Is<IBootable>() || e.Entry.Component.Is<IBootable>() )
+			{
+				var t = this.GetTypeToResolve( e.Entry );
+				var svc = ( IBootable )this.container.Resolve( t );
+				svc.Boot();
+			}
+		}
+
+		Type GetTypeToResolve( IContainerEntry entry ) 
+		{
+			return entry.Service ?? entry.Component;
+		}
+
+		/// <summary>
+		/// Teardowns this facility.
+		/// </summary>
+		/// <param name="container">The container hosting the facility.</param>
+		public void Teardown( IPuzzleContainer container )
+		{
+			container.ComponentRegistered -= new EventHandler<ComponentRegisteredEventArgs>( OnComponentRegistered );
+		}
+	}
+}
