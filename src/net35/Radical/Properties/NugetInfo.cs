53e3cc2887bf394e24aeed0c34ee47ab94749996
Mauro Servienti <mauro@topics.it>
2014-04-26 12:05:45 +0200

    Radical 1.4.3.0-Beta

--- src/net35/Radical/Properties/NugetInfo.cs
+++ src/net35/Radical/Properties/NugetInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 #pragma warning disable 1607
 //This is the build number userd to publish the NuGet package.
-[assembly: AssemblyInformationalVersion( "1.4.3.0-Alfa" )]
+[assembly: AssemblyInformationalVersion( "1.4.3.0-Beta" )]
 #pragma warning restore 1607
