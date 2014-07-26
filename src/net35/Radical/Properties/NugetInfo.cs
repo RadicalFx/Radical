a264ef442eab23a253526e22f795103b39e9e363
Mauro Servienti <mauro@topics.it>
2014-07-26 09:50:18 +0200

    Release: Summer-Party

--- src/net35/Radical/Properties/NugetInfo.cs
+++ src/net35/Radical/Properties/NugetInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 #pragma warning disable 1607
 //This is the build number userd to publish the NuGet package.
-[assembly: AssemblyInformationalVersion( "1.4.3.1-Beta-1" )]
+[assembly: AssemblyInformationalVersion( "1.4.3.1" )]
 #pragma warning restore 1607
