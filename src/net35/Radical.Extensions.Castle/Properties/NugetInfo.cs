5c50fd9ab03438ee88c91f51e76af66c399b9b33
Mauro Servienti <mauro@topics.it>
2014-11-17 13:16:32 +0100

    practicality

--- src/net35/Radical.Extensions.Castle/Properties/NugetInfo.cs
+++ src/net35/Radical.Extensions.Castle/Properties/NugetInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 #pragma warning disable 1607
 //This is the build number used to publish the NuGet package.
-[assembly: AssemblyInformationalVersion( "2.1.0.0-alfa" )]
+[assembly: AssemblyInformationalVersion( "2.1.0.0" )]
 #pragma warning restore 1607
