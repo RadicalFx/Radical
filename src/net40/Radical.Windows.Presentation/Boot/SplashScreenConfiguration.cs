b77baeaf22b42f3db72149f5cc4f6b0d38242a63
Mauro Servienti <mauro@topics.it>
2015-03-30 14:35:19 +0200

    #50 first draft

---
+++ src/net40/Radical.Windows.Presentation/Boot/SplashScreenConfiguration.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+
+namespace Topics.Radical.Windows.Presentation.Boot
+{
+	/// <summary>
+	/// The configuration of the SplashScreen.
+	/// </summary>
+	public class SplashScreenConfiguration
+	{
+		/// <summary>
+		/// SplashScreenConfiguration default constructor.
+		/// </summary>
+		public SplashScreenConfiguration()
+		{
+			this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+			this.SizeToContent = SizeToContent.WidthAndHeight;
+			this.WindowStyle = WindowStyle.None;
+			this.MinimumDelay = 1500;
+			this.SplashScreenViewType = typeof( SplashScreenView );
+
+			this.StartupAsyncWork = obj => System.Threading.Thread.Sleep( this.MinimumDelay );
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public SizeToContent SizeToContent { get; set; }
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public WindowStartupLocation WindowStartupLocation { get; set; }
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public WindowStyle WindowStyle { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public Action<IServiceProvider> StartupAsyncWork { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public Double Height { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public Double Width { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public Int32 MinimumDelay { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public Type SplashScreenViewType { get; set; }
+	}
+}
