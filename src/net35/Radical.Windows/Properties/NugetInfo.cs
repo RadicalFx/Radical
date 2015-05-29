46bf38032c76884181ac2f7b212e60476575e57f
Mauro Servienti <mauro@topics.it>
2015-05-29 17:20:28 +0200

    1.3.16.0-Democracy-Alfa-1

--- src/net35/Radical.Windows/Properties/NugetInfo.cs
+++ src/net35/Radical.Windows/Properties/NugetInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 #pragma warning disable 1607
 //This is the build number userd to publish the NuGet package.
-[assembly: AssemblyInformationalVersion( "1.3.16.0-Democracy-Alfa" )]
+[assembly: AssemblyInformationalVersion( "1.3.16.0-Democracy-Alfa-1" )]
 #pragma warning restore 1607
