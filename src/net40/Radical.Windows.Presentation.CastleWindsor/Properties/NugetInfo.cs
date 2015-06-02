3a5f7c097651615e1972d86f5c0fd275a1e7b4da
Mauro Servienti <mauro@topics.it>
2015-06-02 07:33:04 +0200

    Democracy-Beta-1

--- src/net40/Radical.Windows.Presentation.CastleWindsor/Properties/NugetInfo.cs
+++ src/net40/Radical.Windows.Presentation.CastleWindsor/Properties/NugetInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 #pragma warning disable 1607
 //This is the build number used to publish the NuGet package.
-[assembly: AssemblyInformationalVersion( "1.4.0.0-Democracy-Alfa-2" )]
+[assembly: AssemblyInformationalVersion( "1.4.0.0-Democracy-Beta-1" )]
 #pragma warning restore 1607
