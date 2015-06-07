9d36fb7ccf6c7f6efa89d43a2c79f4c84fee7882
Mauro Servienti <mauro@topics.it>
2015-06-07 09:55:10 +0200

    tests skeleton

--- src/net40/Radical.Windows.Presentation/Regions/SwitchingElementsRegion.cs
+++ src/net40/Radical.Windows.Presentation/Regions/SwitchingElementsRegion.cs
@@ -4,89 +4,89 @@ using Topics.Radical.Windows.Presentation.ComponentModel;
 
 namespace Topics.Radical.Windows.Presentation.Regions
 {
-	/// <summary>
-	/// A base abstract implementation for the <see cref="ISwitchingElementsRegion"/>.
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	public abstract class SwitchingElementsRegion<T> :
-		ElementsRegion<T>,
-		ISwitchingElementsRegion
-		where T : FrameworkElement
-	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="SwitchingElementsRegion&lt;T&gt;"/> class.
-		/// </summary>
-		protected SwitchingElementsRegion()
-		{
+    /// <summary>
+    /// A base abstract implementation for the <see cref="ISwitchingElementsRegion"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class SwitchingElementsRegion<T> :
+        ElementsRegion<T>,
+        ISwitchingElementsRegion
+        where T : FrameworkElement
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SwitchingElementsRegion&lt;T&gt;"/> class.
+        /// </summary>
+        protected SwitchingElementsRegion()
+        {
 
-		}
+        }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="SwitchingElementsRegion&lt;T&gt;"/> class.
-		/// </summary>
-		/// <param name="name">The name.</param>
-		protected SwitchingElementsRegion( String name )
-			: base( name )
-		{
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SwitchingElementsRegion&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        protected SwitchingElementsRegion( String name )
+            : base( name )
+        {
 
-		}
+        }
 
-		/// <summary>
-		/// Gets the content of the active.
-		/// </summary>
-		/// <value>
-		/// The content of the active.
-		/// </value>
-		public abstract DependencyObject ActiveContent { get; }
+        /// <summary>
+        /// Gets the content of the active.
+        /// </summary>
+        /// <value>
+        /// The content of the active.
+        /// </value>
+        public abstract DependencyObject ActiveContent { get; }
 
-		/// <summary>
-		/// Occurs when the active content changes.
-		/// </summary>
-		public event EventHandler<ActiveContentChangedEventArgs> ActiveContentChanged;
+        /// <summary>
+        /// Occurs when the active content changes.
+        /// </summary>
+        public event EventHandler<ActiveContentChangedEventArgs> ActiveContentChanged;
 
-		/// <summary>
-		/// Called when the active content is changed.
-		/// </summary>
-		protected virtual void OnActiveContentChanged()
-		{
-		    var h = this.ActiveContentChanged;
-			if( h != null )
-			{
-				h( this, new ActiveContentChangedEventArgs( this.ActiveContent, this.PreviousActiveContent ) );
-			}
+        /// <summary>
+        /// Called when the active content is changed.
+        /// </summary>
+        protected virtual void OnActiveContentChanged()
+        {
+            var h = this.ActiveContentChanged;
+            if( h != null )
+            {
+                h( this, new ActiveContentChangedEventArgs( this.ActiveContent, this.PreviousActiveContent ) );
+            }
 
-		    if (RegionService.Conventions != null)
-		    {
-		        var vm = RegionService.Conventions
+            if (RegionService.Conventions != null)
+            {
+                var vm = RegionService.Conventions
                     .GetViewDataContext(this.ActiveContent, ViewDataContextSearchBehavior.LocalOnly) as IExpectViewActivatedCallback;
-		        if (vm != null)
-		        {
-		            vm.OnViewActivated();
-		        }
-		    }
+                if (vm != null)
+                {
+                    vm.OnViewActivated();
+                }
+            }
 
-		    if( this.ActiveContent != this.PreviousActiveContent )
-			{
-				this.PreviousActiveContent = this.ActiveContent;
-			}
-		}
+            if( this.ActiveContent != this.PreviousActiveContent )
+            {
+                this.PreviousActiveContent = this.ActiveContent;
+            }
+        }
 
-		/// <summary>
-		/// Gets the content of the previous active.
-		/// </summary>
-		/// <value>
-		/// The content of the previous active.
-		/// </value>
-		public DependencyObject PreviousActiveContent
-		{
-			get;
-			private set;
-		}
+        /// <summary>
+        /// Gets the content of the previous active.
+        /// </summary>
+        /// <value>
+        /// The content of the previous active.
+        /// </value>
+        public DependencyObject PreviousActiveContent
+        {
+            get;
+            private set;
+        }
 
-		/// <summary>
-		/// Activates the specified content.
-		/// </summary>
-		/// <param name="content">The content.</param>
-		public abstract void Activate( DependencyObject content );
-	}
+        /// <summary>
+        /// Activates the specified content.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        public abstract void Activate( DependencyObject content );
+    }
 }
---
+++ src/net45/Test.Radical.Windows.Presentation/Regions/Specialized/TacControlRegionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Radical.Windows.Presentation.Regions.Specialized
+{
+    [TestClass]
+    public class TacControlRegionTests
+    {
+        [TestMethod]
+        [TestCategory( "TabControlRegion" ), TestCategory( "Regions" ), TestCategory( "UI Composition" )]
+        public void TabControlRegion_ActiveContentChanged_should_notify_VM_if_IExpectViewModelActivatedCallback()
+        {
+            Assert.Inconclusive( "To be implemented" );
+        }
+    }
+}
