3ecfb00b25df2ce0f77ddb2df2ec38c9c435013c
Nazareno Manco <nazareno.m@live.com>
2016-03-24 17:13:23 +0100

    Added HasFlag to Enum Extensions for fx 3.5

---
+++ src/net35/Test.Radical/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Radical
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
