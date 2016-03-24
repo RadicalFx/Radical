6194483a39af1f21430481edfa458032e02df708
Nazareno Manco <nazareno.m@live.com>
2016-03-24 14:39:17 +0100

    Added HasFlag Extension on enum for Framework 3.5

---
+++ src/net35/Test.Radical/Extensions/EnumExtensionTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpTestsEx;
+using Topics.Radical;
+
+namespace Test.Radical.Extensions
+{
+    /// <summary>
+    /// Summary description for EnumExtensionTest
+    /// </summary>
+    [TestClass]
+    public class EnumExtensionTest
+    {
+        [Flags]
+        private enum FlaggedTestEnum
+        {
+            None = 0,
+
+            Value1 = 1,
+            Value2 = 2,
+            Value4 = 4,
+            Value8 = 8
+        }
+
+        [TestMethod]
+        public void enumExtensions_HasFlag_checking_set_flag_return_true()
+        {
+            var testCase = FlaggedTestEnum.Value1 | FlaggedTestEnum.Value2;
+            var actual = testCase.HasFlag(FlaggedTestEnum.Value1);
+
+            actual.Should().Be.True();
+        }
+
+        [TestMethod]
+        public void enumExtensions_HasFlag_checking_not_set_flag_return_false()
+        {
+            var testCase = FlaggedTestEnum.Value1 | FlaggedTestEnum.Value2;
+            var actual = testCase.HasFlag(FlaggedTestEnum.Value8);
+
+            actual.Should().Be.False();
+        }
+
+
+    }
+}
