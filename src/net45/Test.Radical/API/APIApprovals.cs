94b744de0d0edeff0762e938a1c2ad65e4d528f5
Mauro Servienti <mauro.servienti@gmail.com>
2017-08-07 15:34:22 +0200

    Add API Approvals tests

---
+++ src/net45/Test.Radical/API/APIApprovals.cs
@@ -0,0 +1,24 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PublicApiGenerator;
+using System.Runtime.CompilerServices;
+using Topics.Radical.ComponentModel.Messaging;
+
+namespace Test.Radical.API
+{
+    [TestClass]
+    public class APIApprovals
+    {
+        [TestMethod]
+        [TestCategory("APIApprovals")]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        [UseReporter(typeof(DiffReporter))]
+        public void Approve_FXV45_API()
+        {
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(IMessageBroker).Assembly);
+
+            Approvals.Verify(publicApi);
+        }
+    }
+}
