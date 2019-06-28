4e4d623c4201e6eefdc51d7416403014dd54f48c
mauroservienti <mauro.servienti@gmail.com>
2019-06-28 10:36:28 +0200

    improve build support

--- targets/Program.cs
+++ targets/Program.cs
@@ -1,45 +1,21 @@
-using System;
-using System.Diagnostics;
 using System.IO;
 using static Bullseye.Targets;
+using static SimpleExec.Command;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
-        Target(
-            "default",
-            Directory.EnumerateFiles("src", "*.sln", SearchOption.AllDirectories),
-            solution => Cmd("dotnet", $"build \"{solution}\" --configuration Debug"));
+        Target("default", DependsOn("test"));
 
-	    Target(
-            "release",
+        Target("build",
             Directory.EnumerateFiles("src", "*.sln", SearchOption.AllDirectories),
-            solution => Cmd("dotnet", $"build \"{solution}\" --configuration Release"));
+            solution => Run("dotnet", $"build \"{solution}\" --configuration Release"));
 
+        Target("test", DependsOn("build"),
+            Directory.EnumerateFiles("src", "*Tests.csproj", SearchOption.AllDirectories),
+            proj => Run("dotnet", $"test \"{proj}\" --configuration Release --no-build"));
+        
         RunTargets(args);
-    }
-
-    private static void Cmd(string fileName, string args)
-    {
-        using (var process = new Process())
-        {
-            process.StartInfo = new ProcessStartInfo
-            {
-                FileName = $"\"{fileName}\"",
-                Arguments = args,
-                UseShellExecute = false,
-            };
-
-            Console.WriteLine($"Running '{process.StartInfo.FileName} {process.StartInfo.Arguments}'...");
-            process.Start();
-
-            process.WaitForExit();
-
-            if (process.ExitCode != 0)
-            {
-                throw new InvalidOperationException($"The command exited with code {process.ExitCode}.");
-            }
-        }
     }
 }
