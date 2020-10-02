40624c8eacd0e84c6ddf1657ef6eaa6abd0eb428
Mauro Servienti <mauro.servienti@gmail.com>
2020-10-02 21:02:11 +0200

    some tests

---
+++ src/Radical.Tests/Helpers/RandomStringsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Radical.Helpers;
+
+namespace Radical.Tests.Helpers
+{
+    [TestClass]
+    public class RandomStringsTests
+    {
+        [TestMethod]
+        public void Generated_string_should_respect_default_min_max_len()
+        {
+            var sut = new RandomStrings();
+            var defaultMinLen = sut.MinLength;
+            var defaultMaxLen = sut.MaxLength;
+            var rndStr = sut.Next();
+            
+            Assert.IsTrue(rndStr.Length >= defaultMinLen);
+            Assert.IsTrue(rndStr.Length <= defaultMaxLen);
+        }
+    }
+}
