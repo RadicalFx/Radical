248f63a433af1f1b38f68ec888c63a8d2ff1fbac
Mauro Servienti <mauro.servienti@gmail.com>
2020-10-03 11:23:40 +0200

    use nameof

--- src/Radical/ComponentModel/ContractAttribute/ContractAttribute.cs
+++ src/Radical/ComponentModel/ContractAttribute/ContractAttribute.cs
@@ -24,7 +24,7 @@
         {
             if (contractInterface == null)
             {
-                throw new ArgumentNullException("contractInterface");
+                throw new ArgumentNullException(nameof(contractInterface));
             }
 
             ContractInterface = contractInterface;
