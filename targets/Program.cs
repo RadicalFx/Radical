7901908840900b1381990487789baaa474813a0e
Mauro Servienti <mauro.servienti@gmail.com>
2022-08-23 17:03:52 +0200

    Use async API

--- targets/Program.cs
+++ targets/Program.cs
@@ -1,22 +1,24 @@
 using System.IO;
+using System.Threading.Tasks;
 using static Bullseye.Targets;
 using static SimpleExec.Command;
 
 internal class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var sdk = new DotnetSdkManager();
+        var dotnet = await sdk.GetDotnetCliPath();
 
         Target("default", DependsOn("test"));
 
         Target("build",
             Directory.EnumerateFiles("src", "*.sln", SearchOption.AllDirectories),
-            solution => Run(sdk.GetDotnetCliPath(), $"build \"{solution}\" --configuration Release"));
+            solution => Run(dotnet, $"build \"{solution}\" --configuration Release"));
 
         Target("test", DependsOn("build"),
             Directory.EnumerateFiles("src", "*Tests.csproj", SearchOption.AllDirectories),
-            proj => Run(sdk.GetDotnetCliPath(), $"test \"{proj}\" --configuration Release --no-build"));
+            proj => Run(dotnet, $"test \"{proj}\" --configuration Release --no-build"));
         
         RunTargetsAndExit(args);
     }
